8a6821897903d5155f812a0d646872ccac21a68d
6yhn <6yhn@fresnou.org>
2023-02-17 14:18:16 -0800

    initial commit

---
+++ whileloops1.js
@@ -0,0 +1,30 @@
+const readLine = require('readline-sync');
+
+let x = 0;
+
+while (x < 26){
+
+console.log(x);
+
+x++;
+
+}
+
+let a = 0;
+
+while (a < 51){
+
+    console.log(a);
+
+    a++;
+
+}
+let b = 0
+
+while (b < -10){
+
+    console.log(b);
+
+    b++;
+
+}
