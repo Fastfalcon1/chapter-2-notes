152ba348b666e02d841dbb7c6dcd33f1f040f6df
6yhn <6yhn@fresnou.org>
2023-02-23 14:18:33 -0800

    initial commit

---
+++ forloops1.js
@@ -0,0 +1,27 @@
+for (let i = 0; i < 151; i++){
+    console.log(i);
+}
+
+for (let i = 0; i <=40; i++){
+
+    console.log(i**2);
+
+}
+
+for (let i = 0; i <=30; i++){
+
+    console.log(i**3);
+
+}
+
+for (let i = 0; i <=700; i++){
+    if(i % 5 == 0) {
+        console.log(i);
+    }
+}
+
+for (let i = 0; i <=700; i++){
+    if(i % 5 == 0) {
+        console.log(i);
+    }
+}
